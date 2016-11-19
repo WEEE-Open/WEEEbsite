773d0b989c637c1b684ee68af757d94637898096
Ludovico Pavesi <ludovico.pavesi@gmail.com>
2016-11-19 21:02:21 +0100

    Favicon

--- templates/base.php
+++ templates/base.php
@@ -11,6 +11,14 @@
 	<meta name="viewport" content="width=device-width">
 	<meta name="generator" content="Corkhammer">
 	<link type="text/css" rel="stylesheet" href="main.css" media="screen">
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="img/apple-touch-icon-144x144.png">
+    <link rel="apple-touch-icon-precomposed" sizes="152x152" href="img/apple-touch-icon-152x152.png">
+    <link rel="icon" type="image/png" href="img/favicon-32x32.png" sizes="32x32">
+    <link rel="icon" type="image/png" href="img/favicon-16x16.png" sizes="16x16">
+    <meta name="application-name" content="WEEE Open">
+    <meta name="msapplication-TileColor" content="#00983A">
+    <meta name="msapplication-TileImage" content="img/mstile-144x144.png">
+
 </head>
 <body>
 	<div id="logo">
