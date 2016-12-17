b5c047a10c4ed086f89bc6f0be01117615c4d9b7
Ludovico Pavesi <ludovico.pavesi@gmail.com>
2016-12-17 19:04:04 +0100

    Ora la build funziona.
    Bonus: il CSS è ancora più minificato di prima.

--- build.php
+++ build.php
@@ -2,4 +2,7 @@
 <?php
 define('INPUT', 'input');
 define('OUTPUT', 'output');
+
+$parsers = ['UnderscoreDotYaml', 'HTMLWithYAMLFrontMatter', 'HTMLWithDefaultTemplate', 'YamlForMarkdown', 'MarkdownWithYAMLFrontMatter', 'Markdown', 'CSSMinify'];
+
 require 'ma/ma.php';
