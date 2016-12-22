a9c635e8ccdadf4d047cd598eb541bfb00118325
Ludovico Pavesi <ludovico.pavesi@gmail.com>
2016-12-22 12:17:50 +0100

    Tab invece che spazi

--- build.php
+++ build.php
@@ -6,44 +6,44 @@ define('OUTPUT', 'output');
 $parsers = ['UnderscoreDotYaml', 'HTMLWithYAMLFrontMatter', 'HTMLWithDefaultTemplate', 'YamlForMarkdown', 'MarkdownWithYAMLFrontMatter', 'Markdown', 'CSSMinify'];
 
 function onMerged($output) {
-    $parser = new \lvps\MechatronicAnvil\Parsers\NoContentPHPTemplate();
-    $index = NULL;
+	$parser = new \lvps\MechatronicAnvil\Parsers\NoContentPHPTemplate();
+	$index = NULL;
 	/** @var \lvps\MechatronicAnvil\Directory $output */
-    $output->walkCallback(function($file) use(&$index) {
+	$output->walkCallback(function($file) use (&$index) {
 		/** @var \lvps\MechatronicAnvil\File $file */
-        if(substr($file->getBasename(), 0, 6) === 'index.') {
-            $index = $file;
-        }
-    });
-    /** @var \lvps\MechatronicAnvil\File $index */
-    if($index === NULL) {
-        throw new LogicException('Missing index file!');
-    }
+		if(substr($file->getBasename(), 0, 6) === 'index.') {
+			$index = $file;
+		}
+	});
+	/** @var \lvps\MechatronicAnvil\File $index */
+	if($index === NULL) {
+		throw new LogicException('Missing index file!');
+	}
 
-    /** @var \lvps\MechatronicAnvil\Directory $blog */
+	/** @var \lvps\MechatronicAnvil\Directory $blog */
 
-    $blog = $output->descendInto('blog');
-    $posts = [];
-    $pinnedPosts = [];
-    $blog->walkCallback(function($file) use (&$posts, &$pinnedPosts) {
-        /** @var \lvps\MechatronicAnvil\File $file */
-        if($file->getDoRender()) { // TODO: what happens if there's a subdirectory?
+	$blog = $output->descendInto('blog');
+	$posts = [];
+	$pinnedPosts = [];
+	$blog->walkCallback(function($file) use (&$posts, &$pinnedPosts) {
+		/** @var \lvps\MechatronicAnvil\File $file */
+		if($file->getDoRender()) { // TODO: what happens if there's a subdirectory?
 			$md = $file->getMetadata();
 			// blog_post: is it a blog post? If not, we won't do anything else.
-            // hidden: hide from list (but still render its page, with next\prev buttons, and add to pinned if needed)
-            // pinned: the 3 most recent pinned posts will be passed to index.html, to be displayed in home page.
+			// hidden: hide from list (but still render its page, with next\prev buttons, and add to pinned if needed)
+			// pinned: the 3 most recent pinned posts will be passed to index.html, to be displayed in home page.
 			if(isset($md['blog_post']) && $md['blog_post'] === true) {
-			    if(!isset($md['hidden']) || $md['hidden'] === false) {
+				if(!isset($md['hidden']) || $md['hidden'] === false) {
 					$posts[] = $file;
-                }
+				}
 				if(isset($md['pinned']) && $md['pinned'] === true) {
-				    $pinnedPosts[] = $file;
-                }
+					$pinnedPosts[] = $file;
+				}
 			}
-        }
-    });
+		}
+	});
 
-    // newest first, oldest last
+	// newest first, oldest last
 	usort($pinnedPosts, function($postA, $postB) {
 		/** @var \lvps\MechatronicAnvil\File $postA */
 		/** @var \lvps\MechatronicAnvil\File $postB */
@@ -57,79 +57,81 @@ function onMerged($output) {
 		]));
 	}
 
-    // oldest first, newest last
-    usort($posts, function($postA, $postB) {
+	// oldest first, newest last
+	usort($posts, function($postA, $postB) {
 		/** @var \lvps\MechatronicAnvil\File $postA */
 		/** @var \lvps\MechatronicAnvil\File $postB */
-        return $postA->getMetadata()['date'] - $postB->getMetadata()['date'];
-    });
+		return $postA->getMetadata()['date'] - $postB->getMetadata()['date'];
+	});
 	if(isset($posts[1])) {
-	    $posts[0]->addMetadataOnTop(new \lvps\MechatronicAnvil\Metadata(['next' => $posts[1]]));
-    }
+		$posts[0]->addMetadataOnTop(new \lvps\MechatronicAnvil\Metadata(['next' => $posts[1]]));
+	}
 	if(isset($posts[count($posts) - 2])) {
 		$posts[count($posts) - 1]->addMetadataOnTop(new \lvps\MechatronicAnvil\Metadata(['prev' => $posts[count($posts) - 2]]));
 	}
 	for($i = 1; $i < count($posts) - 1; $i++) {
-        $posts[$i]->addMetadataOnTop(new \lvps\MechatronicAnvil\Metadata(['next' => $posts[$i+1]]));
-        $posts[$i]->addMetadataOnTop(new \lvps\MechatronicAnvil\Metadata(['prev' => $posts[$i-1]]));
-    }
+		$posts[$i]->addMetadataOnTop(new \lvps\MechatronicAnvil\Metadata(['next' => $posts[$i + 1]]));
+		$posts[$i]->addMetadataOnTop(new \lvps\MechatronicAnvil\Metadata(['prev' => $posts[$i - 1]]));
+	}
 
 	// oldest last, newest first
-    $posts = array_reverse($posts);
+	$posts = array_reverse($posts);
 
 	$ppp = $blog->getMetadata()['postsPerPage'];
 	$pages = [];
-	$pagesCount = (int) ceil(count($posts)/$ppp);
+	$pagesCount = (int) ceil(count($posts) / $ppp);
 	for($page = 0; $page < $pagesCount; $page++) {
-	    if($page === $pagesCount - 1) {
-	        // last page
+		if($page === $pagesCount - 1) {
+			// last page
 			$postsInThisPage = array_slice($posts, $page * $ppp);
-        } else {
+		} else {
 			$postsInThisPage = array_slice($posts, $page * $ppp, $ppp);
 		}
 
 		if($page === 0) {
-	        $thisPage = new \lvps\MechatronicAnvil\File('index.html', $blog);
-        } else {
-	        $thisPage = new \lvps\MechatronicAnvil\File('pagina-'.$page.'.html', $blog);
-        }
+			$thisPage = new \lvps\MechatronicAnvil\File('index.html', $blog);
+		} else {
+			$thisPage = new \lvps\MechatronicAnvil\File('pagina-' . $page . '.html', $blog);
+		}
 
-        $thisPage->setParser($parser);
+		$thisPage->setParser($parser);
 		$thisPage->addMetadataOnTop($blog->getMetadata());
-        $thisPage->addMetadataOnTop(new \lvps\MechatronicAnvil\Metadata([
-            'posts' => $postsInThisPage,
-            'template' => 'blogpage.php',
-        ]));
+		$thisPage->addMetadataOnTop(new \lvps\MechatronicAnvil\Metadata([
+			'posts' => $postsInThisPage,
+			'template' => 'blogpage.php',
+		]));
 
-        $pages[] = $thisPage;
-    }
+		$pages[] = $thisPage;
+	}
 
-    foreach($pages as $i => $page) {
+	foreach($pages as $i => $page) {
 		if($i === 0) {
 			if($pagesCount > 1) {
 				$pages[$i]->addMetadataOnTop(new \lvps\MechatronicAnvil\Metadata([
 					'pagination' => [
-                        'next' => $pages[$i + 1],
-                    ]
+						'next' => $pages[$i + 1],
+					]
 				]));
 			}
 		} else if($i === $pagesCount - 1) {
 			if($pagesCount > 1) {
 				$pages[$i]->addMetadataOnTop(new \lvps\MechatronicAnvil\Metadata([
 					'pagination' => [
-                        'prev' => $pages[$i - 1],
-                    ]
+						'prev' => $pages[$i - 1],
+					]
 				]));
 			}
 		} else {
 			$pages[$i]->addMetadataOnTop(new \lvps\MechatronicAnvil\Metadata([
 				'pagination' => [
-				    'next' => $pages[$i + 1],
-				    'prev' => $pages[$i - 1],
-                ]
+					'next' => $pages[$i + 1],
+					'prev' => $pages[$i - 1],
+				]
 			]));
 		}
-    }
-};
+	}
+}
+
+;
 
 require 'ma/ma.php';
